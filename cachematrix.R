7d0914f9fe6fe691cb90715bbc36078c561f789e
pd1010 <des_pc@yahoo.com>
2015-11-18 19:22:01 -0800

    first commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,58 @@
-## Put comments here that give an overall description of what your
-## functions do
+# makeCacheMatrix creates a special "matrix" object that can cache its inverse.
 
-## Write a short comment describing this function
+# 1 set the value of the matrix 
+# 2 get the value of the matrix 
+# 3 set the value of inverse of the matrix 
+# 4 get the value of inverse of the matrix 
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix()) { 
+     inv <- NULL 
+     set <- function(y) { 
+         x <<- y 
+         inv <<- NULL 
+     } 
+     get <- function() x 
+     setinverse <- function(inverse) inv <<- inverse 
+     getinverse <- function() inv 
+     list(set=set, get=get, setinverse=setinverse, getinverse=getinverse) 
+	} 
 
-}
+# cacheSolve function returns the inverse of the matrix. 
+# This function assumes that the matrix is always invertible.
+# It first checks if the inverse has already been computed. 
+# If so, it gets the result and skips the computation. 
+# If not, it computes the inverse, sets the value in the cache via 
+# setinverse function. 
+ 
+cacheSolve <- function(x, ...) { 
+     inv <- x$getinverse() 
+     if(!is.null(inv)) { 
+         message("getting cached data") 
+         return(inv) 
+	} 
+     data <- x$get() 
+     inv <- solve(data) 
+     x$setinverse(inv) 
+     inv 
+	}
 
 
-## Write a short comment describing this function
+# x = rbind(c(1, 1/2), c(1/2, 1))
+# m = makeCacheMatrix(x)
+# m$get()
+#    [,1] [,2]
+# [1,]  1.0  0.5
+# [2,]  0.5  1.0
+# No cache in the first run
+# cacheSolve(m)
+#          [,1]       [,2]
+# [1,]  1.3333333 -0.6666667
+# [2,] -0.6666667  1.3333333
+# Retrieving cached data in the 2nd run
+# cacheSolve(m)
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+#           [,1]       [,2]
+# [1,]  1.3333333 -0.6666667
+# [2,] -0.6666667  1.3333333
+
+ 
